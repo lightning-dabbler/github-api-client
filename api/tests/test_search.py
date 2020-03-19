2225bd06078a8b413ef3b068c0799866deac4eba
Captain Lightning <[Omitted]>
2020-03-19 00:32:45 -0400

    incorporated mamba for testing

--- api/tests/test_search.py
+++ api/tests/test_search.py
@@ -1,4 +1,12 @@
 import search
+from mamba import description,context,it
 
-x = search.get_users('light+repos:>5','stars','asc')
-print(x[0],len(x[1]))
+with description('Get Users') as self:
+    with context('Function utilizes /search/users endpoint'):
+        with it('Returns a tuple: tuple[0]-> status code & tuple[0] -> list of dictionaries of metadata'):
+            x = search.get_users('light+repos:>5','stars','asc')
+            assert type(x[0]) == int
+            assert type(x[1]) == list
+            if x[1]: assert type(x[1][0]) == dict
+
+            
