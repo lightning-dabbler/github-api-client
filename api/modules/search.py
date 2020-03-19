1b631569f9f5e2453ec6b7c5d3d32b52fa708d3a
Captain Lightning <[Omitted]>
2020-03-19 18:04:29 -0400

    Updates to search module

--- api/modules/search.py
+++ api/modules/search.py
@@ -3,6 +3,56 @@ import requests
 
 GITHUB_API = os.getenv('GITHUB_API')
 
+def search_lazy(endpoint,query,sort=None,order=None,page=1,per_page=100):
+    """
+    `endpoint:str = 'users','repositories','commits'`
+
+    `query:str`
+    
+    - users:
+    ------
+    https://developer.github.com/v3/search/#search-users
+    https://help.github.com/en/github/searching-for-information-on-github/searching-users
+    
+    - repositories:
+    -----
+    https://developer.github.com/v3/search/#search-repositories
+    https://help.github.com/en/github/searching-for-information-on-github/searching-for-repositories
+    
+    - commits:
+    -----
+    https://developer.github.com/v3/search/#search-commits
+    https://help.github.com/en/github/searching-for-information-on-github/searching-commits
+
+    `returns generator`
+    """
+    header = {'Content-Type': 'application/json'}
+    url = f'{GITHUB_API}/search/{endpoint}?q={query}&per_page={per_page}'
+    if sort:
+        url = f'{url}&sort={sort}'
+    if order:
+        url = f'{url}&order={order}'
+    
+    done = False
+    total_returned = 0
+    while not done:
+        request_url = f'{url}&page={page}'
+        response = requests.request(
+            'GET',request_url,headers=header)
+        headers = dict(response.headers)
+        status_code = response.status_code
+        response = response.json()
+
+        if status_code != 200:
+            yield status_code,[],headers
+            return
+        total_count = response['total_count']
+        page+=1
+        results = response['items']
+        yield status_code,results,headers
+        total_returned+=per_page
+        if total_returned >= total_count:
+            done = True
 
 def search(endpoint,query,sort=None,order=None,page=1,per_page=100,strict=False):
     """
@@ -25,86 +75,21 @@ def search(endpoint,query,sort=None,order=None,page=1,per_page=100,strict=False)
     https://developer.github.com/v3/search/#search-commits
     https://help.github.com/en/github/searching-for-information-on-github/searching-commits
     """
-    header = {'Content-Type': 'application/json'}
-    url = f'{GITHUB_API}/search/{endpoint}?q={query}&per_page={per_page}'
-    if sort:
-        url = f'{url}&sort={sort}'
-    if order:
-        url = f'{url}&order={order}'
-    
+
     results =[]
+
     if not strict:
-        done = False
-        total_returned = 0
-        while not done:
-            response = requests.request(
-                'GET',f'{url}&page={page}',headers=header)
-            headers = dict(response.headers)
-            status_code = response.status_code
-            response = response.json()
-            if status_code != 200:
-                return status_code,results,headers
-            total_count = response['total_count']
-            page+=1
-            results.extend(response['items'])
-            total_returned+=per_page
-            if total_returned >= total_count:
-                done = True
+        responses = search_lazy(endpoint,query,sort=sort,order=order,page=page,per_page=per_page) 
+        for response in responses:
+            status_code = response[0]
+            items = response[1]
+            headers = response[2]
+            results.extend(items)
     else:
-        response = requests.request(
-            'GET',f'{url}&page={page}',headers=header)
-        headers = dict(response.headers)
-        status_code = response.status_code
-        response = response.json()
-        if status_code != 200:
-            return status_code,results,headers
-        return status_code,response['items'],headers
+        responses = search_lazy(endpoint,query,sort=sort,order=order,page=page,per_page=per_page) 
+        response = next(responses)
+        status_code = response[0]
+        items = response[1]
+        headers = response[2]
+        results.extend(items)
     return status_code,results,headers
-
-def search_lazy(endpoint,query,sort=None,order=None,page=1,per_page=100):
-    """
-    `endpoint:str = 'users','repositories','commits'`
-
-    `query:str`
-    
-    - users:
-    ------
-    https://developer.github.com/v3/search/#search-users
-    https://help.github.com/en/github/searching-for-information-on-github/searching-users
-    
-    - repositories:
-    -----
-    https://developer.github.com/v3/search/#search-repositories
-    https://help.github.com/en/github/searching-for-information-on-github/searching-for-repositories
-    
-    - commits:
-    -----
-    https://developer.github.com/v3/search/#search-commits
-    https://help.github.com/en/github/searching-for-information-on-github/searching-commits
-    """
-    header = {'Content-Type': 'application/json'}
-    url = f'{GITHUB_API}/search/{endpoint}?q={query}&per_page={per_page}'
-    if sort:
-        url = f'{url}&sort={sort}'
-    if order:
-        url = f'{url}&order={order}'
-    
-    done = False
-    total_returned = 0
-    while not done:
-        response = requests.request(
-            'GET',f'{url}&page={page}',headers=header)
-        headers = dict(response.headers)
-        status_code = response.status_code
-        response = response.json()
-
-        if status_code != 200:
-            yield status_code,[],headers
-            return
-        total_count = response['total_count']
-        page+=1
-        results = response['items']
-        yield status_code,results,headers
-        total_returned+=per_page
-        if total_returned >= total_count:
-            done = True
