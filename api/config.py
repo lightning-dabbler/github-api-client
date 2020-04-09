9d73970d1f334710491c637155e36f6ecd00c4b7
Captain Lightning <osarodionirabor@gmail.com>
2020-04-09 00:28:26 -0400

    integrated redis with python api backend (flask-session & redis); Tested and stable

--- api/config.py
+++ api/config.py
@@ -1,6 +1,10 @@
 import os
+import redis
 
 APP_ENV = os.environ.get('APP_ENV','development')
+SESSION_TYPE = 'redis'
+PERMANENT_SESSION_LIFETIME = 60*60
+SESSION_REDIS = redis.from_url(os.environ.get('REDIS_URL_NET'))
 
 if APP_ENV == 'production':
     FLASK_ENV = APP_ENV
@@ -8,4 +12,5 @@ if APP_ENV == 'production':
 else:
     FLASK_ENV = 'development'
     DEBUG = True
+    
 
