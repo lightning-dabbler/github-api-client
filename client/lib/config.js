cea7cd42c7a66fc104de0ee9c036cb660f069914
Captain Lightning <osarodionirabor@gmail.com>
2020-04-09 00:40:11 -0400

    update to config.js

--- client/lib/config.js
+++ client/lib/config.js
@@ -1,6 +1,6 @@
 const environment = process.env.NODE_ENV
 
 module.exports = {
-    redis_url: environment === 'prod' ? process.env.REDIS_URL_WEB : process.env.REDIS_URL_NET,
+    redis_url: process.env.REDIS_URL_NET,
     api_url: environment === 'prod' ? process.env.GITHUB_API_WEB : process.env.GITHUB_API_NET
 }
