b00bd4f987a7295d03d27105b2bdf0f4423b3c19
Captain Lightning <osarodionirabor@gmail.com>
2020-05-19 23:30:30 -0400

    updates to gunicorn port(s)

--- api/gunicorn.conf.py
+++ api/gunicorn.conf.py
@@ -1,7 +1,8 @@
 """gunicorn WSGI server configuration."""
 from multiprocessing import cpu_count
+import os
 
-bind = "0.0.0.0:2064"
+bind = f"0.0.0.0:{os.environ.get('X_GUNICORN_PORT',2064)}"
 workers = cpu_count() * 2 + 1
 loglevel = 'info'
 max_requests = 10000
