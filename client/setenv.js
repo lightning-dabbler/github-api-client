3a2bad7f2dda8ab6b3b264904b67e4b109d32e67
Captain Lightning <osarodionirabor@gmail.com>
2020-05-19 22:30:21 -0400

    minor Refinements

--- client/setenv.js
+++ client/setenv.js
@@ -1,8 +1,6 @@
 const environment = process.env.NODE_ENV
 
 if (environment === 'prod'){
-    // const API_URL = process.env.WEB_URL
-    // console.log(`API_URL=${API_URL}/api`)
     console.log(`API_URL=/api`)
 }
 else {
